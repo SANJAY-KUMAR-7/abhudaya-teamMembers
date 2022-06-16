b67023f4eace7dfbb5dcd7e8c578de6c1799ada1
SANJAY-KUMAR-7 <83998914+SANJAY-KUMAR-7@users.noreply.github.com>
2022-06-16 19:07:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,294 @@
+function founding_research_team_1(){
+    document.getElementById("more-info-container-1").setAttribute("style","display:block;");
+    document.getElementById("more-info-1").setAttribute("style","display:none;");  
+}
+
+function founding_research_team_close_1(){
+    document.getElementById("more-info-container-1").setAttribute("style","display:none;");
+    document.getElementById("more-info-1").setAttribute("style","display:block;"); 
+}
+
+function connection_team_1(){
+    document.getElementById("more-info-container-2").setAttribute("style","display:block;");
+    document.getElementById("more-info-2").setAttribute("style","display:none;");  
+}
+
+function connection_team_close_1(){
+    document.getElementById("more-info-container-2").setAttribute("style","display:none;");
+    document.getElementById("more-info-2").setAttribute("style","display:block;"); 
+}
+function communication_team_1(){
+    document.getElementById("more-info-container-3").setAttribute("style","display:block;");
+    document.getElementById("more-info-3").setAttribute("style","display:none;");  
+}
+
+function communication_team_close_1(){
+    document.getElementById("more-info-container-3").setAttribute("style","display:none;");
+    document.getElementById("more-info-3").setAttribute("style","display:block;"); 
+}
+//2nd row
+function founding_research_team_2(){
+    document.getElementById("more-info-container-4").setAttribute("style","display:block;");
+    document.getElementById("more-info-4").setAttribute("style","display:none;");  
+}
+
+function founding_research_team_close_2(){
+    document.getElementById("more-info-container-4").setAttribute("style","display:none;");
+    document.getElementById("more-info-4").setAttribute("style","display:block;"); 
+}
+
+function connection_team_2(){
+    document.getElementById("more-info-container-5").setAttribute("style","display:block;");
+    document.getElementById("more-info-5").setAttribute("style","display:none;");  
+}
+
+function connection_team_close_2(){
+     document.getElementById("more-info-container-5").setAttribute("style","display:none;");
+    document.getElementById("more-info-5").setAttribute("style","display:block;"); 
+}
+function communication_team_2(){
+    document.getElementById("more-info-container-6").setAttribute("style","display:block;");
+    document.getElementById("more-info-6").setAttribute("style","display:none;");  
+}
+
+function communication_team_close_2(){
+    document.getElementById("more-info-container-6").setAttribute("style","display:none;");
+    document.getElementById("more-info-6").setAttribute("style","display:block;"); 
+}
+/*********CORE****************** */
+function core_research_team_1(){
+    document.getElementById("core_more-info-container-1").setAttribute("style","display:block;");
+    document.getElementById("core_more-info-1").setAttribute("style","display:none;");  
+}
+
+function core_research_team_close_1(){
+    document.getElementById("core_more-info-container-1").setAttribute("style","display:none;");
+    document.getElementById("core_more-info-1").setAttribute("style","display:block;"); 
+}
+
+function core_connection_team_1(){
+    document.getElementById("core_more-info-container-2").setAttribute("style","display:block;");
+    document.getElementById("core_more-info-2").setAttribute("style","display:none;");  
+}
+
+function core_connection_team_close_1(){
+    document.getElementById("core_more-info-container-2").setAttribute("style","display:none;");
+    document.getElementById("core_more-info-2").setAttribute("style","display:block;"); 
+}
+function core_communication_team_1(){
+    document.getElementById("core_more-info-container-3").setAttribute("style","display:block;");
+    document.getElementById("core_more-info-3").setAttribute("style","display:none;");  
+}
+
+function core_communication_team_close_1(){
+    document.getElementById("core_more-info-container-3").setAttribute("style","display:none;");
+    document.getElementById("core_more-info-3").setAttribute("style","display:block;"); 
+}
+function core_research_team_2(){
+    document.getElementById("core_more-info-container-4").setAttribute("style","display:block;");
+    document.getElementById("core_more-info-4").setAttribute("style","display:none;");  
+}
+//2nd row
+function core_research_team_close_2(){
+    document.getElementById("core_more-info-container-4").setAttribute("style","display:none;");
+    document.getElementById("core_more-info-4").setAttribute("style","display:block;"); 
+}
+
+function core_connection_team_2(){
+    document.getElementById("core_more-info-container-5").setAttribute("style","display:block;");
+    document.getElementById("core_more-info-5").setAttribute("style","display:none;");  
+}
+
+function core_connection_team_close_2(){
+     document.getElementById("core_more-info-container-5").setAttribute("style","display:none;");
+    document.getElementById("core_more-info-5").setAttribute("style","display:block;"); 
+}
+function core_communication_team_2(){
+    document.getElementById("core_more-info-container-6").setAttribute("style","display:block;");
+    document.getElementById("core_more-info-6").setAttribute("style","display:none;");  
+}
+
+function core_communication_team_close_2(){
+    document.getElementById("core_more-info-container-6").setAttribute("style","display:none;");
+    document.getElementById("core_more-info-6").setAttribute("style","display:block;"); 
+}
+
+function core_communication_team_3(){
+    document.getElementById("core_more-info-container-7").setAttribute("style","display:block;");
+    document.getElementById("core_more-info-7").setAttribute("style","display:none;");  
+}
+
+function core_communication_team_close_3(){
+    document.getElementById("core_more-info-container-7").setAttribute("style","display:none;");
+    document.getElementById("core_more-info-7").setAttribute("style","display:block;"); 
+}
+function core_communication_team_4(){
+    document.getElementById("core_more-info-container-8").setAttribute("style","display:block;");
+    document.getElementById("core_more-info-8").setAttribute("style","display:none;");  
+}
+
+function core_communication_team_close_4(){
+    document.getElementById("core_more-info-container-8").setAttribute("style","display:none;");
+    document.getElementById("core_more-info-8").setAttribute("style","display:block;"); 
+}
+/********* Communication and Connections Team *****************/
+function communication_connection_team_1(){
+    document.getElementById("communication_connection_more-info-container-1").setAttribute("style","display:block;");
+    document.getElementById("communication_connection_more-info-1").setAttribute("style","display:none;");  
+}
+
+function communication_connection_team_close_1(){
+    document.getElementById("communication_connection_more-info-container-1").setAttribute("style","display:none;");
+    document.getElementById("communication_connection_more-info-1").setAttribute("style","display:block;"); 
+}
+
+function communication_connection_team_2(){
+    document.getElementById("communication_connection_more-info-container-2").setAttribute("style","display:block;");
+    document.getElementById("communication_connection_more-info-2").setAttribute("style","display:none;");  
+}
+
+function communication_connection_team_close_2(){
+    document.getElementById("communication_connection_more-info-container-2").setAttribute("style","display:none;");
+    document.getElementById("communication_connection_more-info-2").setAttribute("style","display:block;"); 
+}
+
+function communication_connection_team_3(){
+    document.getElementById("communication_connection_more-info-container-3").setAttribute("style","display:block;");
+    document.getElementById("communication_connection_more-info-3").setAttribute("style","display:none;");  
+}
+
+function communication_connection_team_close_3(){
+    document.getElementById("communication_connection_more-info-container-3").setAttribute("style","display:none;");
+    document.getElementById("communication_connection_more-info-3").setAttribute("style","display:block;"); 
+}
+
+function communication_connection_team_4(){
+    document.getElementById("communication_connection_more-info-container-4").setAttribute("style","display:block;");
+    document.getElementById("communication_connection_more-info-4").setAttribute("style","display:none;");  
+}
+
+function communication_connection_team_close_4(){
+    document.getElementById("communication_connection_more-info-container-4").setAttribute("style","display:none;");
+    document.getElementById("communication_connection_more-info-4").setAttribute("style","display:block;"); 
+}
+
+function communication_connection_team_5(){
+    document.getElementById("communication_connection_more-info-container-5").setAttribute("style","display:block;");
+    document.getElementById("communication_connection_more-info-5").setAttribute("style","display:none;");  
+}
+
+function communication_connection_team_close_5(){
+    document.getElementById("communication_connection_more-info-container-5").setAttribute("style","display:none;");
+    document.getElementById("communication_connection_more-info-5").setAttribute("style","display:block;"); 
+}
+
+function communication_connection_team_6(){
+    document.getElementById("communication_connection_more-info-container-6").setAttribute("style","display:block;");
+    document.getElementById("communication_connection_more-info-6").setAttribute("style","display:none;");  
+}
+
+function communication_connection_team_close_6(){
+    document.getElementById("communication_connection_more-info-container-6").setAttribute("style","display:none;");
+    document.getElementById("communication_connection_more-info-6").setAttribute("style","display:block;"); 
+}
+
+/* *************   website team **************/
+function website_team_1(){
+    document.getElementById("website_team_more-info-container-1").setAttribute("style","display:block;");
+    document.getElementById("website_team_more-info-1").setAttribute("style","display:none;");  
+}
+
+function website_team_close_1(){
+    document.getElementById("website_team_more-info-container-1").setAttribute("style","display:none;");
+    document.getElementById("website_team_more-info-1").setAttribute("style","display:block;"); 
+}
+
+function website_team_2(){
+    document.getElementById("website_team_more-info-container-2").setAttribute("style","display:block;");
+    document.getElementById("website_team_more-info-2").setAttribute("style","display:none;");  
+}
+
+function website_team_close_2(){
+    document.getElementById("website_team_more-info-container-2").setAttribute("style","display:none;");
+    document.getElementById("website_team_more-info-2").setAttribute("style","display:block;"); 
+}
+
+function website_team_3(){
+    document.getElementById("website_team_more-info-container-3").setAttribute("style","display:block;");
+    document.getElementById("website_team_more-info-3").setAttribute("style","display:none;");  
+}
+
+function website_team_close_3(){
+    document.getElementById("website_team_more-info-container-3").setAttribute("style","display:none;");
+    document.getElementById("website_team_more-info-3").setAttribute("style","display:block;"); 
+}
+
+function website_team_4(){
+    document.getElementById("website_team_more-info-container-4").setAttribute("style","display:block;");
+    document.getElementById("website_team_more-info-4").setAttribute("style","display:none;");  
+}
+
+function website_team_close_4(){
+    document.getElementById("website_team_more-info-container-4").setAttribute("style","display:none;");
+    document.getElementById("website_team_more-info-4").setAttribute("style","display:block;"); 
+}
+
+function website_team_5(){
+    document.getElementById("website_team_more-info-container-5").setAttribute("style","display:block;");
+    document.getElementById("website_team_more-info-5").setAttribute("style","display:none;");  
+}
+
+function website_team_close_5(){
+    document.getElementById("website_team_more-info-container-5").setAttribute("style","display:none;");
+    document.getElementById("website_team_more-info-5").setAttribute("style","display:block;"); 
+}
+
+function website_team_6(){
+    document.getElementById("website_team_more-info-container-6").setAttribute("style","display:block;");
+    document.getElementById("website_team_more-info-6").setAttribute("style","display:none;");  
+}
+
+function website_team_close_6(){
+    document.getElementById("website_team_more-info-container-6").setAttribute("style","display:none;");
+    document.getElementById("website_team_more-info-6").setAttribute("style","display:block;"); 
+}
+/**********************  RESEARCH TEAM   ************************/
+function research_team_1(){
+    document.getElementById("research_team_more-info-container-1").setAttribute("style","display:block;");
+    document.getElementById("research_team_more-info-1").setAttribute("style","display:none;");  
+}
+
+function research_team_close_1(){
+    document.getElementById("research_team_more-info-container-1").setAttribute("style","display:none;");
+    document.getElementById("research_team_more-info-1").setAttribute("style","display:block;"); 
+}
+
+function research_team_2(){
+    document.getElementById("research_team_more-info-container-2").setAttribute("style","display:block;");
+    document.getElementById("research_team_more-info-2").setAttribute("style","display:none;");  
+}
+
+function research_team_close_2(){
+    document.getElementById("research_team_more-info-container-2").setAttribute("style","display:none;");
+    document.getElementById("research_team_more-info-2").setAttribute("style","display:block;"); 
+}
+
+function research_team_3(){
+    document.getElementById("research_team_more-info-container-3").setAttribute("style","display:block;");
+    document.getElementById("research_team_more-info-3").setAttribute("style","display:none;");  
+}
+
+function research_team_close_3(){
+    document.getElementById("research_team_more-info-container-3").setAttribute("style","display:none;");
+    document.getElementById("research_team_more-info-3").setAttribute("style","display:block;"); 
+}
+
+function research_team_4(){
+    document.getElementById("research_team_more-info-container-4").setAttribute("style","display:block;");
+    document.getElementById("research_team_more-info-4").setAttribute("style","display:none;");  
+}
+
+function research_team_close_4(){
+    document.getElementById("research_team_more-info-container-4").setAttribute("style","display:none;");
+    document.getElementById("research_team_more-info-4").setAttribute("style","display:block;"); 
+}
